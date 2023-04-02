56c231e0ad5791720191e723a3b199eaf9625753
shshuvo23 <shshuvo63@gmail.com>
2023-04-02 10:48:18 +0600

    slider change

--- resources/views/website/home/index.blade.php
+++ resources/views/website/home/index.blade.php
@@ -1,71 +1,30 @@
 @extends('website.master')
-
-
 @section('body')
-<div class="axil-main-slider-area main-slider-style-1">
+<div class="axil-main-slider-area main-slider-style-5">
     <div class="container">
-        <div class="row align-items-center">
-            <div class="col-lg-5 col-sm-6">
-                <div class="main-slider-content">
-                    <div class="slider-content-activation-one">
-                        @foreach ($recent_products as $recent_product)
-                        <div class="single-slide slick-slide" data-sal="slide-up" data-sal-delay="400" data-sal-duration="800">
-                            <span class="subtitle"><i class="fas fa-fire"></i> Hot Deal In This Week</span>
-                            <h1 class="title">{{$recent_product->product_name}}</h1>
-                            <div class="slide-action">
-                                <div class="shop-btn">
-                                    <a href="shop.html" class="axil-btn btn-bg-white"><i class="fal fa-shopping-cart"></i>Shop Now</a>
-                                </div>
-                                <div class="item-rating">
-                                    <div class="thumb">
-                                        <ul>
-                                            <li><img src="{{asset('/')}}website/assets/images/others/author1.png" alt="Author"></li>
-                                            <li><img src="{{asset('/')}}website/assets/images/others/author2.png" alt="Author"></li>
-                                            <li><img src="{{asset('/')}}website/assets/images/others/author3.png" alt="Author"></li>
-                                            <li><img src="{{asset('/')}}website/assets/images/others/author4.png" alt="Author"></li>
-                                        </ul>
-                                    </div>
-                                    <div class="content">
-                                        <span class="rating-icon">
-                                    <i class="fas fa-star"></i>
-                                    <i class="fas fa-star"></i>
-                                    <i class="fas fa-star"></i>
-                                    <i class="fas fa-star"></i>
-                                    <i class="fal fa-star"></i>
-                                </span>
-                                        <span class="review-text">
-                                    <span>100+</span> Reviews
-                                        </span>
-                                    </div>
-                                </div>
-                            </div>
+
+        <div class="slider-box-wrap">
+            <div class="slider-activation-two axil-slick-dots">
+                @foreach ( $recent_products as $recent_product )
+                <div class="single-slide slick-slide">
+                    <div class="main-slider-content">
+                        <span class="subtitle"><i class="fas fa-fire"></i> Hot Deal In This Week</span>
+                        <h1 class="title">{{$recent_product->product_name}}</h1>
+                        <div class="shop-btn">
+                            <a href="" class="axil-btn btn-bg-white"><i class="fal fa-shopping-cart"></i> Buy Product</a>
                         </div>
-                        @endforeach
+                    </div>
+                    <div class="main-slider-thumb">
+                        <img src="{{asset($recent_product->image)}}" alt="Product">
                     </div>
                 </div>
-            </div>
-            <div class="col-lg-7 col-sm-6">
-                <div class="main-slider-large-thumb">
-                    <div class="slider-thumb-activation-one axil-slick-dots">
-                        @foreach ( $recent_products as $recent_product )
-                        <div class="single-slide slick-slide" data-sal="slide-up" data-sal-delay="600" data-sal-duration="1500">
-                            <img src="{{asset($recent_product->image)}}" alt="Product" height="600" width="100">
-                            <div class="product-price">
-                                <span class="text">From</span>
-                                <span class="price-amount">{{$recent_product->price}}</span>
-                            </div>
-                        </div>
-                        @endforeach
-                    </div>
-                </div>
+                @endforeach
             </div>
         </div>
+
     </div>
-    <ul class="shape-group">
-        <li class="shape-1"><img src="{{asset('/')}}website/assets/images/others/shape-1.png" alt="Shape"></li>
-        <li class="shape-2"><img src="{{asset('/')}}website/assets/images/others/shape-2.png" alt="Shape"></li>
-    </ul>
 </div>
+
 <!-- Start Categorie Area  -->
 <div class="axil-categorie-area bg-color-white axil-section-gapcommon">
     <div class="container">
@@ -88,6 +47,8 @@
         </div>
     </div>
 </div>
+
+
 @endsection
 
 @section('product')
